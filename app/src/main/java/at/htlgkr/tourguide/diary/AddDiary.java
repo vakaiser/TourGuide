2aecbfa959932d191175ed5f76ce9a618133d1c7
Vanessa Kaiser <vanessa.kaiser.03@gmail.com>
2020-06-12 11:36:12 +0200

    tmp AddDiary-Intent

---
+++ app/src/main/java/at/htlgkr/tourguide/diary/AddDiary.java
@@ -0,0 +1,16 @@
+package at.htlgkr.tourguide.diary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import at.htlgkr.tourguide.R;
+
+public class AddDiary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_diary);
+    }
+}
